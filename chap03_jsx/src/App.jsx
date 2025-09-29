9e8ed8df975ebf0018095b3ba338f448c1723941
SHIN SUNGJIN <pxp13716@gmail.com>
2025-09-29 16:51:35 +0900

    .

--- chap03_jsx/src/App.jsx
+++ chap03_jsx/src/App.jsx
@@ -1,3 +1,128 @@
+// View는 함수 또는 Class로 정의 가능. Class는 16.8 버전 이후로 거의 사용 안함
+// 이러한 조각 View를 컴포넌트라 한다.
+
+/*
+JSX
+  1. 반드시 종료 태그가 존재해야 한다.
+    <br></br> OR <br />
+  2. JSX는 ""(문자열)로 묶지 않는다. 문자열은 그대로 문자로 표시된다
+  3. JSX의 태그 속성명은 JavaScript 속명 방식을 따른다
+    class => className, <label for=".."> => <label htmlFor="..">
+  4. View의 반환(JSX)은 반드시 단일 태그만 가능하다
+    에러 => 요소가 2개 반환됨
+    return <h1>Hello</h1>
+      <div>World!!</div>
+    
+    정상 => 요소가 1개만 반환됨
+    return <div>
+      <h1>Hello</h1>
+      <div>World!!</div>
+    </div>
+*/
+
+/*
+function First() {
+  // return으로 JSX 문법의 View를 반환한다
+  return <h1>Hello World!!!</h1>
+}
+export default First;
+*/
+
+/*
+const elem = <h1>Hello World!!!</h1>
+function App() {
+  return elem;
+}
+export default App;
+*/
+
+// View의 반환(JSX)은 반드시 단일 태그만 가능하다
+/*
+// Error
+const App = () => {
+  return <h1>Hello World!!!</h1>
+  <div>Good Morning</div>
+}
+export default App;
+*/
+
+/*
+const App = () => {
+  return <div>
+    <h1>Hello World!!!</h1>
+    <div>Good Morning</div>
+  </div>
+}
+export default App;
+*/
+
+/*
+const elem = <div>
+  <h1>Hello World!!!</h1>
+  <div>Good Morning 2</div>
+</div>;
+
+const App = () => {
+  return elem;
+}
+export default App;
+*/
+
+/*
+// return 다음에 태그가 오는걸 방지할 목적으로 ( )로 묶어 사용한다
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World!!!</h1>
+      <div>Good Morning</div>
+    </div>
+  )
+}
+export default App;
+*/
+
+// 1. 반드시 종료 태그가 존재해야 한다.
+// 3. JSX의 태그 속성명은 JavaScript 속명 방식을 따른다
+
+// 이 컴포넌트에서만 사용할 목적의 CSS 파일 import (하지만 전역으로 표시됨)
+// 먼저 올라온 동일 이름의 CSS의 클래스, ID를 덮어쓴다.
+// import './App.css';
+
+// img, svg 파일 import
+import viteLogo from '/svg/vite.svg';         // dist에 svg 폴더가 통으로 복사
+import reactLogo from './assets/react.svg';   // assets 이미지는 리소스로 관리
+
+// 분리된 View를 import
+import A01Comp from './components/A01Comp';
+
+const App = () => {
+  return (
+    <div className="m-3">
+      <h1>Hello World!!!</h1>
+
+      <div>Good Morning</div>
+      <br />
+
+      <div className='mb-3'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} alt="vite" height="100" />
+        </a>
+        <img src={reactLogo} alt="react" height="100" />
+      </div>
+
+      <div className="mb-3">
+        위 실장은 이날 오후 용산 대통령실에서 열린 기자간담회에서 최근 조지아주에서 발생한 한국인 근로자 구금 문제와 관련해 “비자 문제를 조속히 해결하겠다”고 밝혔다. 그는 “비자 제도 개선을 위한 협의가 이제 막 시작됐다”며 “예측 가능하도록 개선할 것이고, 나아가 새로운 비자 카테고리 신설도 추진할 것”이라고 말했다. 이어 “전문직 비자 분야까지 확대할 수 있겠지만, 아직은 협상이 필요하다”며 “가급적 조속한 성과를 내기 위해 노력하겠다”고 설명했다.
+      </div>
+
+      <A01Comp></A01Comp>
+
+    </div>
+  )
+}
+export default App;
+
+
+/*
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -33,3 +158,4 @@ function App() {
 }
 
 export default App
+*/
--- chap03_jsx/src/main.jsx
+++ chap03_jsx/src/main.jsx
@@ -1,10 +1,23 @@
+// 프로젝트 전체에 대한 전역 설정
+// Router 설정
+// Store 설정
+// 프로젝트 전체에서 사용할 CSS 관리
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// 프로젝트 전체에서 사용할 통 CSS 파일
+// import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 유지보수 측면에서 View를 분리해서 다른 파일에 정의 후 import
 import App from './App.jsx'
 
+// render => JSX 형태의 문법을 JavaScript 객체로 변환 (Babel, Esbuild)
 createRoot(document.getElementById('root')).render(
+  // StrictMode => 잘못된 사용법 등에 대한 경고 출력. Build하면 삭제된다.
+  // JSX(View)를 반환하는 함수를 React에서는 사용자 정의 태그 형태로 사용
+  // JSX => JavaScript XML
   <StrictMode>
-    <App />
+    <App></App>
   </StrictMode>,
 )
