0522dd7565743d26e72b41d9c1f1ed3915b40039
SHIN SUNGJIN <pxp13716@gmail.com>
2025-09-29 10:56:00 +0900

    .

--- chap01_module/src/import.js
+++ chap01_module/src/import.js
@@ -1,15 +1,26 @@
-// 개별 export 된 요소를 import
+/*
+개별 export 된 요소를 import
+1. { } 내부에 선언되는 변수명은 export 한 변수명과 동일한 이름으로 정의해야 한다.
+2. HTML에서 type을 module로 정의해야 한다
+  type="text/javascript" => type="module"
+3. 이미 사용되고 있는 변수명이 있다면 as로 이름 변경 가능. 변경 후에는 변경된 이름만 사용 가능
+*/
+import { name as nick, age, check, arr, user, add } from './exportOne.js'
 
-console.log(age);
+// 한 이름으로 묶어서 사용
+import * as one from './exportOne.js'
+// console.log(one);
+
+const name = 'Module';
 
 const dom = `
   <div>
-    Name: <br>
-    Age: <br>
-    Check: <br>
-    Array: <br>
-    User: <br>
-    onAdd: <br>
+    Name: ${name} / ${nick} / ${one.name}<br>
+    Age: ${age} / ${one.age}<br>
+    Check: ${check}<br>
+    Array: ${arr[0]} / ${one.arr[1]} / ${arr[2]}<br>
+    User: ${user.name} / ${one.user.age} / ${user.address}<br>
+    onAdd: ${add(20, 30)}<br>
 
     <hr>
 
